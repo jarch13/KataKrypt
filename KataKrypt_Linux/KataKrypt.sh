d299336677ab53c49b091628656f3101a6dd3cb4
jarch13 <142846035+jarch13@users.noreply.github.com>
2024-09-30 19:46:02 -0700

    KataKrypt_Linux
    This is KataKrypt optimized for Linux operating systems.

---
+++ KataKrypt_Linux/KataKrypt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script to run KataKrypt.py
+
+# Navigate to the script's directory to ensure relative paths work
+cd "$(dirname "$0")"
+
+# Run the Python script
+python3 KataKrypt.py
